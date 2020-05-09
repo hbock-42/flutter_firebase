4f87d480c9911d0f2a7bd19b60649d1043a2ef57
Hugo Bock <h.bock@easyrecrue.com>
2020-05-09 16:22:16 +0000

    refactor

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/models/user.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -38,26 +39,4 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
-
-class User {
-  final String firstName;
-  final String lastName;
-  final DocumentReference reference;
-  final Timestamp _timeStamp;
-  DateTime get birthDate => _timeStamp.toDate();
-
-  User.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['firstName'] != null),
-        assert(map['lastName'] != null),
-        assert(map['birthDate'] != null),
-        firstName = map['firstName'],
-        lastName = map['lastName'],
-        _timeStamp = map['birthDate'];
-
-  User.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
-
-  @override
-  String toString() => "User<$firstName:$lastName:$birthDate>";
 }
