a8e625a55fbf98bdea969844744498f035dd9ad2
Hugo Bock <h.bock@easyrecrue.com>
2020-05-09 15:58:48 +0000

    get users from firestore

---
+++ lib/home_page.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: Firestore.instance.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return LinearProgressIndicator();
+          return _buildUserList(context, snapshot.data.documents);
+        });
+  }
+
+  Widget _buildUserList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final user = User.fromSnapshot(data);
+    return Padding(
+      key: ValueKey(user.firstName + user.lastName),
+      padding: EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(user.lastName),
+          trailing: Text(user.firstName),
+          onTap: () => print(user),
+        ),
+      ),
+    );
+  }
+}
+
+class User {
+  final String firstName;
+  final String lastName;
+  final DocumentReference reference;
+  final Timestamp _timeStamp;
+  DateTime get birthDate => _timeStamp.toDate();
+
+  User.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['firstName'] != null),
+        assert(map['lastName'] != null),
+        assert(map['birthDate'] != null),
+        firstName = map['firstName'],
+        lastName = map['lastName'],
+        _timeStamp = map['birthDate'];
+
+  User.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "User<$firstName:$lastName:$birthDate>";
+}
